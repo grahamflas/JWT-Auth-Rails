4e3a2357271f2b878018c7c556a7d3f7f7f03c72
Graham Flaspoehler <grahamflas@gmail.com>
2020-06-06 16:50:47 -0400

    Implement methods to encode/ decode tokens in ApplicationController
    - Placed in application controller, because controllers throughout the application will need to authenticate users
    
    - To access a protected routes/ resources, users should send the JWT in the authorization header:
    
        fetch('http://localhost:3000/api/v1/profile', {
          method: 'GET',
          headers: {
            Authorization: `Bearer <token>`
          }
        })

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  def encode_token(payload)
+    # payload = {beef: 'steak'}
+    # JWT.encode(payload, 'my_secret')=> "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ.OIsGX8Y2XTQk6gF_4LuMRrHrkNqiVAe9Un4A5KlqX8E"
+
+    JWT.encode(payload, 'my_secret')
+  end
+
+  def decode_token(token)
+    # token = "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ.OIsGX8Y2XTQk6gF_4LuMRrHrkNqiVAe9Un4A5KlqX8E"
+    # JWT.decode(token, 'my_secret') => [{"beef"=>"steak"}, {"alg"=>"HS256"}]
+
+    JWT.decode(token, 'my_secret').first
+  end
 end
